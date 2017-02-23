10f4f99cdba0708d2f27390c0e8cc8e79239d4b5
callosaurus <callumdavies421@gmail.com>
2017-02-23 11:09:34 -0800

    first commit

---
+++ WavyCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WavyCollectionView
+//
+//  Created by Callum Davies on 2017-02-23.
+//  Copyright © 2017 Callum Davies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WavyCollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WavyCollectionView
+//
+//  Created by Callum Davies on 2017-02-23.
+//  Copyright © 2017 Callum Davies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
